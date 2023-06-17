dcb1961994a261d6a57544a8edadc8220909e987
Didik2584 <114987079+Didik2584@users.noreply.github.com>
2023-06-17 07:24:32 +0300

    Update config.go

--- config/config.go
+++ config/config.go
@@ -1,52 +1,7 @@
 package config
 
-import (
-	"log"
-	"os"
-	"strconv"
+import "os"
 
-	"github.com/joho/godotenv"
-)
-
-func GetenvInt(envName string) int {
-	strEnv := os.Getenv(envName)
-	i, err := strconv.Atoi(strEnv)
-	if err != nil {
-		log.Printf("error parsing env %v to int: %v\n", envName, err)
-	}
-	return i
-}
-
-type config struct {
-	AppPort    string
-	DBHost     string
-	DBUsername string
-	DBPassword string
-	DBName     string
-	DBPort     int
-	JWTKey     []byte
-}
-
-var Config *config
-
-func Init() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Printf("error loading .env file: %v\n", err)
-	}
-
-	if Config == nil {
-		Config = &config{
-			AppPort:    os.Getenv("PORT"),
-			DBHost:     os.Getenv("DB_HOST"),
-			DBUsername: os.Getenv("DB_USERNAME"),
-			DBPassword: os.Getenv("DB_PASSWORD"),
-			DBName:     os.Getenv("DB_NAME"),
-			DBPort:     GetenvInt("DB_PORT"),
-			JWTKey:     []byte(os.Getenv("JWT_SECRET")),
-		}
-	}
-}
 var (
 	// BaseURL is the base url of the server
 	BaseURL = os.Getenv("BASE_URL")
